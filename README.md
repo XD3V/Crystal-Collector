0012f2ef59c8ce4dee07f29018d0876d8923902c
Xavier Davis <xdavis1@icloud.com>
2019-08-17 12:33:32 -0700

    last commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Crystal-Collector
+
+just to let you know I was not able to make it work on the javaScript file
--- javaScript.js
+++ javaScript.js
@@ -1,62 +1,107 @@
-
-
-
-var targetNumber = 53;
-
-  $("#number-to-guess").text(targetNumber);
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
   var counter = 0;
 
-  // Now for the hard part. Creating multiple crystals each with their own unique number value.
+  var wins = 0;
+
+  var losses = 0; 
+
+  var targetNumber = '';
+
+  var targetNumber = Math.floor(Math.random() *101)+19;
+
+  $("#number-to-guess").text(targetNumber);
+  // Now for the hard part. Creating multiple latte each with their own unique number value.
 
 
+
+ 
+
+      
   // We begin by expanding our array to include four options.
-  var numberOptions1 = [10, 5, 3, 7];
+  var numberOptions1 =  [20, 5, 1, 10];
 
   // Next we create a for loop to create crystals for every numberOption.
   for (var i = 0; i < numberOptions1.length; i++) {
 
-    // For each iteration, we will create an imageCrystal
+    // For each iteration, we will create an imageLatte
     var imageLatte = $("<img>");
 
-    // First each crystal will be given the class ".crystal-image".
+    // First each latte will be given the class ".latte-image".
     // This will allow the CSS to take effect.
     imageLatte.addClass("coffeepic1");
 
-    // Each imageCrystal will be given a src link to the crystal image
+    // Each imageLatte will be given a src link to the latte image
     imageLatte.attr("src","https://images.unsplash.com/photo-1475241404975-c3ae90fdd9e6?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&dl=brigitte-tohm-BnrKDRn5sjg-unsplash.jpg,");
 
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
+    // Each imageLatte will be given a data attribute called data-latteValue.
     // This data attribute will be set equal to the array value.
     imageLatte.attr("data-lattevalue", numberOptions1[i]);
 
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+    // Lastly, each latte image (with all it classes and attributes) will get added to the page.
     $("#coffeepic1").append(imageLatte);
-  }
 
-  // This time, our click event applies to every single crystal on the page. Not just one.
+    }
+
+
+      
+  // This time, our click event applies to every single latte on the page. Not just one.
   $(".coffeepic1").on("click", function() {
 
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+    // Determining the latte's value requires us to extract the value from the data attribute.
+    // Using the $(this) keyword specifies that we should be extracting the latte value of the clicked latte.
+    // Using the .attr("data-lattevalue") allows us to grab the value out of the "data-lattevalue" attribute.
     // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
 
-    var latteValue = ($(this).attr("data-latteValue"));
-    LatteValue = parseInt(LatteValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
-
+    var latteValue = ($(this).attr("data-lattevalue"));
+    latteValue = parseInt(latteValue);
+    // We then add the latteValue to the user's "counter" which is a global variable.
+    // Every click, from every latte adds to the global counter.
+    
+    counter += latteValue;
     // All of the same game win-lose logic applies. So the rest remains unchanged.
-    alert("New score: " + counter);
 
+    $("#count-tracker-div").html("Your total score is:"+ "   "+ counter);
     if (counter === targetNumber) {
       alert("You win!");
+      wins++;
+      gameTime ();
     }
 
-    else if (counter >= targetNumber) {
+    else if (counter > targetNumber) {
       alert("You lose!!");
+      losses++;
+      gameTime ();
     }
 
+function gameTime (){ 
+  var targetNumber = '';
+
+  var targetNumber = Math.floor(Math.random() *101)+19;
+
+  $("#number-to-guess").text(targetNumber);
+
+   $("#loss-tracker-div").html("losses: " +losses);
+
+    $("#win-tracker-div").html("Wins: " + wins);
+
+    $("#count-tracker-div").html("New score: " + counter);
+    
+
+    imageLatte.attr("data-lattevalue", numberOptions1[i]);
+
+    $("#coffeepic1").append(imageLatte);
+
+    
+
+    latteValue = parseInt(latteValue);
+
+    counter = 0;
+   
+  }
+
+  
+
+    
+    
   });
